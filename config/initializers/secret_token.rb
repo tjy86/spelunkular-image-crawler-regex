2e4c4264421180d7dfb4c57b53d84d307075dade
tjy86 <thomasjyang@gmail.com>
2012-12-19 14:28:20 -0500

    asdfasdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpelunkularImageCrawlerRegex::Application.config.secret_token = 'f887e0df949d5a612bab66b80fe22ade6edb9fcfe3951c62779a123ff2e90d1ddb678e9a3a2143d4cb872106d62ce6a1c7926f3651be20c369af02b94839c649'
