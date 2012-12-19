b1e09466f874b32bcae9c3bab753bbf986213058
tjy86 <thomasjyang@gmail.com>
2012-12-19 17:50:28 -0500

    trying to go deeper

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,37 +7,55 @@ class HomeController < ApplicationController
     Image.delete_all
     Link.delete_all
 
-    # begin
-      html = HTTParty.get(params[:url])
-    #   rescue
-    #   return
-    # end
-
-
+    depth = params[:depth].to_i
 
     url_regex = /<a .*?href.*?['"]([^'"]*)['"].*?>\w*<\/a>/m
     image_regex = /<img .*?src.*?['"]([^'"]*)['"].*?>/m
-    urls = html.scan(url_regex)  #this is an array of all of the urls on the page
-    urls.flatten!
-    images = html.scan(image_regex)  #This is an array of all of the images on the page
-    images.flatten!
 
-    images.each do |image|
-      if URI.parse(image).relative?
-        image = params[:url] +image
+    Link.create(:url => params[:url], :depth => 0)
+
+    urls = nil
+
+    i = 0
+
+    while (i < depth)
+
+      links = Link.where(:depth => i)
+
+      links.each do |link|
+          html = HTTParty.get(link.url)
+
+          urls = html.scan(url_regex)  #this is an array of all of the urls on the page
+          urls.flatten!
+
+          images = html.scan(image_regex)  #This is an array of all of the images on the page
+          images.flatten!
+
+          images.each do |image|
+            if URI.parse(image).relative?
+              image = params[:url] +image
+            end
+            i = Image.new
+            i.url = image
+            i.save
+          end
       end
-      i = Image.new
-      i.url = image
-      i.save
+
+      i =+ 1
+
+      urls.each do |url|
+        if URI.parse(url).relative?
+          url = params[:url] + url
+        end
+        u = Link.new
+        u.url = url
+        u.depth = i
+        u.save
+      end
     end
 
-    urls.each do |url|
-      u = Link.new
-      u.url = url
-      u.save
-    end
-binding.pry
     render :json => Image.all
+
   end
 
 end
