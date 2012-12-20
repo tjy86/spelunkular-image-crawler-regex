7e2e4d132b250fb9be4688b88f97f9e310429ce9
Zahra Jabini <zahra.arch@gmail.com>
2012-12-20 13:45:13 -0500

    performance

--- test/performance/homepage_test.rb
+++ test/performance/homepage_test.rb
@@ -11,6 +11,14 @@ class HomepageTest < ActionDispatch::PerformanceTest
   end
 
   def test_scrape_web_1_level_deep
-    post '/', :post => { :url => 'http://apple.com', :depth => '1' }
+    post '/', :url => 'http://apple.com', :depth => '1'
+  end
+
+  def test_scrape_web_2_level_deep
+    post '/', :url => 'http://apple.com', :depth => '2'
+  end
+
+  def test_scrape_web_3_level_deep
+    post '/', :url => 'http://apple.com', :depth => '3'
   end
 end
